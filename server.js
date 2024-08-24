11a66e7d054e35ec3ffe8685a625296ee9372d16
rizon <redwantamim525@gmail.com>
2024-08-24 14:26:09 +0600

    add vercel.json

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
+// Import required modules
 const express = require('express');
 const cors = require('cors');
 const { connectDB } = require('./src/config/database');
 const clientRoutes = require('./src/routes/clientRoutes');
 const projectRoutes = require('./src/routes/projectRoutes');
 
+
+// Initialize Express app
 const app = express();
 const port = process.env.PORT || 5000;
 
+
+// Middleware
 app.use(express.json());
 app.use(cors());
 
+// Connect to MongoDB
 connectDB();
 
+
+// Set up routes
 app.use('/client', clientRoutes);
 app.use('/projects', projectRoutes);
 
@@ -19,6 +27,20 @@ app.get("/", (req, res) => {
   res.send("JMC technology Asset Management");
 });
 
+
+  
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+
+
+
+
+
+
+
+
+
+
+
+
